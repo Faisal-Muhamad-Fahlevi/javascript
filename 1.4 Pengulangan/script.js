d1c7751212b9be7a7c08dad7b4002a28e9b00558
Faisal-Muhamad-Fahlevi <faisalmfahlevi@gmail.com>
2024-04-05 10:06:23 +0700

    Latihan pengulangan

--- 1.4 Pengulangan/script.js
+++ 1.4 Pengulangan/script.js
@@ -24,3 +24,26 @@ do {
 
 
 // Infinite Loop = Pengulangan tak terbatas
+
+// 1. Buat prompt untuk input nilai
+const n = prompt("Masukan nilai N : ")
+
+
+// 2. Inisialisai nilai awal jumlah
+let jumlah =0;
+
+// 3. Inisialisasi nilai awal perulangan
+let i = 1;
+
+// 4. Eksekusi perulangan
+/*while (i <= n) {
+  jumlah = jumlah + i;
+  i++:
+}*/
+
+for ( let i = 1; i <= n; i++) {
+  jumlah = jumlah + i;
+}
+
+// 5. cetak hasil di console
+console.log("Hasil penjumlahan deret 1 sampai dengan " + n + " adalah " + jumlah)
---
+++ Latihan Perulangan3_Faisal_Muhamad_Fahlevi/script.js
@@ -0,0 +1,15 @@
+const Ng = prompt ("Masukan anak ayam: ");
+console.log(" Anak ayam ada "+ N);
+
+for(let i = N; i >=1; i--) {
+    if(i===1) {
+        console.log(" Anak ayam turun "+ i +" mati satu tinggal induknya ");
+    } else {
+        console.log(" Anak ayam turun "+ i +" mati satu tinggal "+(i-1));
+    }
+}
+
+
+
+
+

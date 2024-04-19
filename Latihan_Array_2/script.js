1cb9c14c6f467511b004a39de9875f8d14d2a4a8
Faisal-Muhamad-Fahlevi <faisalmfahlevi@gmail.com>
2024-04-19 16:22:57 +0700

    LatihanArray2

--- Latihan_Array_2/script.js
+++ Latihan_Array_2/script.js
@@ -1,30 +1,33 @@
-const nilaiMahasiswa = [36, 66, 82, 53, 76, 17, 50, 38, 93, 29, 78, 57, 22, 53, 78, 38, 20, 46, 80, 79];
-
-function tentukanIndexNilai(nilai) {
+const nilaiMahasiswa = [
+    36, 66, 82, 53, 76, 17, 50, 38, 93, 29, 78, 57, 22, 53, 78, 38, 20, 46, 80,
+    79,
+  ];
+  
+  function cariIndex(nilai) {
+      let indexArr = [];
     if (nilai > 80) {
-        return 'A';
+      indexArr.push("A");
     } else if (nilai >= 70 && nilai <= 80) {
-        return 'B';
+      indexArr.push("B");
     } else if (nilai >= 55 && nilai < 70) {
-        return 'C';
+      indexArr.push("C");
     } else if (nilai >= 45 && nilai < 55) {
-        return 'D';
+      indexArr.push("D");
     } else {
-        return 'E';
+      indexArr.push("E");
     }
-}
-
-function tampilkanNilaiDanIndex(nilaiArray) {
-    console.log("===========================================");
+    return indexArr;
+  }
+  function liatIndex(nilaiArray) {
+    console.log("============================================");
     console.log("No\t\t\tNilai Ujian\t\t\tIndex Nilai");
-    console.log("===========================================");
+    console.log("============================================");
     for (let i = 0; i < nilaiArray.length; i++) {
-        const nilai = nilaiArray[i];
-        const indexNilai = tentukanIndexNilai(nilai);
-        console.log((i+1) + "\t\t\t\t\t" + nilai + "\t\t\t\t\t" + indexNilai);
+      const nilai = nilaiArray[i];
+      const indexNilai = cariIndex(nilai);
+      console.log(i + 1 + "\t\t\t\t\t" + nilai + "\t\t\t\t\t" + indexNilai);
     }
-    console.log("==========================================");
-
-}
-tampilkanNilaiDanIndex(nilaiMahasiswa);
+    console.log("============================================");
+  }
+  liatIndex(nilaiMahasiswa);  
     
